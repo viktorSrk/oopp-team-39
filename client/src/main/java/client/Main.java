f09aa098f52b704ed393487057a85186518e157c
LeonBraszczyński <L.M.Braszczynski@student.tudelft.nl>
2023-03-09 18:36:34 +0100

    Set up for AddList Popup

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,6 +20,7 @@ import static com.google.inject.Guice.createInjector;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import client.scenes.AddListCtrl;
 import com.google.inject.Injector;
 
 import client.scenes.AddQuoteCtrl;
@@ -42,8 +43,9 @@ public class Main extends Application {
 
         var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
         var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
+        var addList = FXML.load(AddListCtrl.class, "client", "scenes", "AddList.fxml");
 
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
-        mainCtrl.initialize(primaryStage, overview, add);
+        mainCtrl.initialize(primaryStage, overview, add, addList);
     }
 }
