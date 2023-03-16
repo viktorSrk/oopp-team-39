af038f289d49490f24dcedde2734a9bef039b8c0
Marco Smidt <m.a.smidt@student.tudelft.nl>
2023-03-16 18:37:49 +0100

    Last checkstyle change.

--- client/src/test/java/client/utils/TestServerUtils.java
+++ client/src/test/java/client/utils/TestServerUtils.java
@@ -8,7 +8,8 @@ public class TestServerUtils extends ServerUtils {
     public List<commons.List> lists = new ArrayList<>();
     public final List<String> calledMethods = new ArrayList<>();
 
-    // TODO: I've hardcoded the lists field, since the add lists function is not fully integrated yet
+    // TODO: I've hardcoded the lists field,
+    //  since the add lists function is not fully integrated yet
     public TestServerUtils() {
         lists.add(new commons.List("a"));
         lists.add(new commons.List("b"));
