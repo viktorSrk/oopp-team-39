c81aa445828195d8779d7343f2e892867a5d085e
Senne Drent <s.drent@student.tudelft.nl>
2023-03-28 17:31:25 +0200

    unused imports :(

--- client/src/main/java/client/scenes/BoardCtrl.java
+++ client/src/main/java/client/scenes/BoardCtrl.java
@@ -8,8 +8,6 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import com.google.inject.Injector;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
