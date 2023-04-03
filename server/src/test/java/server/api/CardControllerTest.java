39e720e3553333c0d6b2ea817fde848e9e9e7022
Gido Vitner <G.Vitner@student.tudelft.nl>
2023-04-03 18:04:26 +0200

    Tested get controllers

--- server/src/test/java/server/api/CardControllerTest.java
+++ server/src/test/java/server/api/CardControllerTest.java
@@ -4,8 +4,11 @@ import commons.Board;
 import commons.Card;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -40,27 +43,54 @@ class CardControllerTest {
     }
 
     @Test
+    void getEmptyListOfAllCards() {
+        var actual = sut.getAllCards();
+        assertTrue(repo.calledMethods.contains("findAll"));
+        assertEquals(Collections.emptyList(), actual);
+    }
+
+    @Test
+    void getNonEmptyListOfAllCards() {
+        Card card = new Card("a");
+        sut.addCard(card, 0L);
+        var actual = sut.getAllCards();
+        assertTrue(repo.calledMethods.contains("findAll"));
+        assertEquals(List.of(card), actual);
+    }
+
+    @Test
+    void getCardByNegativeId() {
+        var responseEntity = sut.getCardById(-1L);
+        assertEquals(ResponseEntity.badRequest().build(), responseEntity);
+    }
+
+    @Test
+    void getCardByNonExistingId() {
+        var responseEntity = sut.getCardById(1L);
+        assertEquals(ResponseEntity.badRequest().build(), responseEntity);
+    }
+
+    @Test
+    void getCardById() {
+        Card card = new Card("a");
+        sut.addCard(card, card.getId());
+        var responseEntity = sut.getCardById(card.getId());
+        assertTrue(repo.calledMethods.contains("findById"));
+        assertEquals(ResponseEntity.ok(card), responseEntity);
+    }
+
+    @Test
+    void addMessage() {
+        // TODO: After addCard is properly tested
+    }
+
+    @Test
     void cannotAddNullCard() {
         Card card = null;
         var actual = sut.addCard(card, 1L);
         assertEquals(BAD_REQUEST, actual.getStatusCode());
         assertTrue(repo.cards.size() == 0);
     }
-
-
-    @Test
-    void getAllCards() {
-        var actual = sut.getAllCards();
-        assertTrue(repo.calledMethods.contains("findAll"));
-        var emptyList = new ArrayList<Card>();
-        assertEquals(actual, emptyList);
-
-        Card card = new Card("a");
-        sut.addCard(card, 0L);
-        actual = sut.getAllCards();
-        assertTrue(actual.contains(card));
-    }
-
 
     @Test
     void addCard() {
